36e579c01d35c33dec50900ba2f1042d6783ef5b
vpro16513428 <vpro16513428@yahoo.com.tw>
2016-04-08 22:18:36 +0800

    增加從thinkspeak GET JSON的功能(純String)
    (cherry picked from commit 534d0716842bbcb25991de9faeacc95b38044d37)

--- app/src/main/java/com/nutccsie/nutc_fds/MainActivity.java
+++ app/src/main/java/com/nutccsie/nutc_fds/MainActivity.java
@@ -1,7 +1,14 @@
 package com.nutccsie.nutc_fds;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new getJson().execute();
+    }
+
+    private class getJson extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                URL url = new URL("http://api.thingspeak.com/channels/96545/fields/1.json?api_key=5NE777ZJOKKJ1GJT");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                String jsonString= reader.readLine();
+                reader.close();
+
+                Log.d("Tag_in:",jsonString);
+                return null;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
+
+
