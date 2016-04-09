37f5a6d4d06591aa6c7e365d8695a9098116a4a9
vpro16513428 <vpro16513428@yahoo.com.tw>
2016-04-09 23:14:33 +0800

    JSON 功能完善

--- app/src/main/java/com/nutccsie/nutc_fds/MainActivity.java
+++ app/src/main/java/com/nutccsie/nutc_fds/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -17,30 +20,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new getJson().execute();
+        new getjson().execute();//下這一行getjson才會做動作
     }
-
-    private class getJson extends AsyncTask<Void,Void,Void>{
+    protected class getjson extends AsyncTask<Void, Void, Object[]> {
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Object[] doInBackground(Void... params) {
             try {
-
+                //region 從thinkspeak獲取JSON資料(純String)存成JSONArray的型態在jsonArray
                 URL url = new URL("http://api.thingspeak.com/channels/96545/fields/1.json?api_key=5NE777ZJOKKJ1GJT");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.connect();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
                 String jsonString= reader.readLine();
                 reader.close();
+                JSONArray jsonArray =new  JSONObject(jsonString).getJSONArray("feeds");//取得feeds的陣列
 
-                Log.d("Tag_in:",jsonString);
-                return null;
+                int max=0;//最大
+                float percent;//百分比
+
+                //掃過所有feed取得最大值 和最大值除以最後一個feed值的百分比
+                for (int i = 0; i < jsonArray.length();i++){
+                    if (Integer.parseInt(jsonArray.getJSONObject(i).getString("field1")) > max) {
+                        max=Integer.parseInt(jsonArray.getJSONObject(i).getString("field1"));
+                    }
+                }
+                percent=(float) Integer.parseInt(jsonArray.getJSONObject(jsonArray.length()-1).getString("field1"))/max;
+                //endregion
+
+                //Log.d("Tag_in:",jsonArray.getJSONObject(1).getString("field1"));
+                Object[] res = new Object[2];
+                res[0]=max;
+                res[1]=percent;
+                return res;
 
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
         }
+
+        @Override
+        protected void onPostExecute(Object[] res) {
+            super.onPostExecute(res);
+            //res[0] 是最大值 ->Integer型態
+            //res[1] 是最後一個feed值和最大值得相除 ->float型態
+            Log.d("max=",res[0].toString());
+            Log.d("percent=",res[1].toString());
+            //可以在這裡使用res[0]和res[1]去改變UI的值
+            //...
+        }
     }
+
 }
 
 
